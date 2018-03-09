f7bffea68448c0134e8bdf2a38a699aa0aa7fbfb
Doug Cole <idugcoal@gmail.com>
2018-03-08 21:12:26 -0800

    Adds deck.go and deck_test.go

--- deck.go
+++ deck.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Create a new type of deck which is a slice of strings
+// Create a new type of 'deck' which is a slice of strings
 type deck []string
 
 func (d deck) print() {
